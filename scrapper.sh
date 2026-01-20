2f0619e74008ba781c284594b2ed647c8defd6c3
A T <anton@adsen-media.com>
2026-01-20 23:53:17 +0200

    fix bold

--- scrapper.sh
+++ scrapper.sh
@@ -1733,7 +1733,8 @@ for ((idx=${#ADS_DATA_LIST[@]}-1; idx>=0; idx--)); do
         echo "--> add_to_add___: $add_to_add"
         full_message="${full_message}${add_to_add}"$'\n\n'
 
-        add_to_add=$(echo "$add_to_add" | sed -E 's/([A-Z_]+\:)/<b>\1</b> /g')
+        #add_to_add=$(echo "$add_to_add" | sed -E 's/([A-Z_]+\:)/<b>\1</b> /g')
+        add_to_add=$(echo "$add_to_add" | sed -E 's/([A-Z_]+\:)/<b>\1<\/b> /g')
         #add_to_add=$(htmldecode "${add_to_add}")
         echo "--> add_to_add: $add_to_add"
         send_info=1
